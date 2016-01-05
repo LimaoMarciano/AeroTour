174fe7a8b93a9fcae20fa7447e21e13253fb6aa3
MarceloSilva <marceloapsilva@gmail.com>
2016-01-05 01:52:34 -0200

    - New airplane script separating plane parts

--- AeroRace/AeroRace/Assets/Scripts/Airplane/AirplaneBehaviour.cs
+++ AeroRace/AeroRace/Assets/Scripts/Airplane/AirplaneBehaviour.cs
@@ -9,21 +9,6 @@ public class AirplaneBehaviour : MonoBehaviour {
 	public GameObject thrustPosition;
 	public Vector3 thrustVector;
 	public GameObject centerOfMassPosition;
-	[Header("Lift")]
-	public float wingLiftMultiplier = 1;
-	public float wingLiftCoefficient = 4;
-	public float wingCriticalAngleOfAttack = 15;
-	public float wingAngleOffset = 5;
-	public GameObject wingLiftPosition;
-	public GameObject leftWingSpeedSensorObj;
-	public GameObject rightWingSpeedSensorObj;
-	[Header("Tail wing")]
-	public float tailLiftMultiplier = -1;
-	public float tailLiftCoefficient = 0.44f;
-	public float tailCriticalWingOfAttack = 15;
-	public float tailAngleOffset = 0;
-	public GameObject tailLiftPosition;
-	public GameObject tailWingSpeedSensorObj;
 	[Header("Flaps")]
 	public float flapLiftMultiplier = 1;
 	public float flapLiftCoefficient = 0.3f;
@@ -34,10 +19,12 @@ public class AirplaneBehaviour : MonoBehaviour {
 	public float upDragCoefficient = 1;
 	public float sideDragCoefficient = 0.5f;
 
+	public AirSpeedSensor leftWingSpeedSensor;
+
 	//Variables
 	private Rigidbody rb;
 	private AirSpeedSensor rightWingSpeedSensor;
-	private AirSpeedSensor leftWingSpeedSensor;
+//	private AirSpeedSensor leftWingSpeedSensor;
 	private AirSpeedSensor tailWingSpeedSensor;
 
 	private float leftWingLift;
@@ -74,10 +61,6 @@ public class AirplaneBehaviour : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		rb = transform.root.GetComponentInChildren<Rigidbody>();
-		rightWingSpeedSensor = rightWingSpeedSensorObj.GetComponent<AirSpeedSensor>();
-		leftWingSpeedSensor = leftWingSpeedSensorObj.GetComponent<AirSpeedSensor>();
-		tailWingSpeedSensor = tailWingSpeedSensorObj.GetComponent<AirSpeedSensor>();
-		initialCriticalAoA = wingCriticalAngleOfAttack;
 
 
 //		wingLiftCoefficient = CalculateLiftCoefficient(wingLiftAtStall, wingStallSpeed);
@@ -101,17 +84,8 @@ public class AirplaneBehaviour : MonoBehaviour {
 		rb.AddTorque(transform.right * vInput * 8000);
 
 		Vector3 velocity = rb.velocity;
-		leftWingAoA = CalculateAngleOfAttack(velocity, leftWingSpeedSensor.AirSpeed(), wingAngleOffset);
-		rightWingAoA = CalculateAngleOfAttack(velocity, rightWingSpeedSensor.AirSpeed(), wingAngleOffset);
-		tailWingAoA = CalculateAngleOfAttack(velocity, tailWingSpeedSensor.AirSpeed(), tailAngleOffset);
-
-		leftWingAngleCoefficient = CalculateAngleCoefficient(leftWingAoA, wingCriticalAngleOfAttack, isFlapOn);
-		rightWingAngleCoefficient = CalculateAngleCoefficient(rightWingAoA, wingCriticalAngleOfAttack, isFlapOn);
-		tailWingAngleCoefficient = CalculateAngleCoefficient(tailWingAoA, tailCriticalWingOfAttack, isFlapOn);
 
 		ApplyThrust();
-		ApplyWingLift();
-		ApplyTailLift();
 		CalculateDrag();
 
 	}
@@ -124,19 +98,6 @@ public class AirplaneBehaviour : MonoBehaviour {
 		Vector3 thrust;
 		thrust = thrustLevel * thrustForce * thrustVector;
 		rb.AddRelativeForce (thrust);
-	}
-
-	private float CalculateLift (Vector3 airSpeed, float liftMultiplier, float liftCoefficient, float angleCoefficient) {
-		float liftForce;
-
-		if (airSpeed.z >= 0)
-			liftForce = Mathf.Pow(airSpeed.z, 2) * liftCoefficient * liftMultiplier * 0.5f * angleCoefficient;
-		else
-			liftForce = 0;
-
-		if (liftForce > 80000)
-			liftForce = 80000;
-		return liftForce;
 	}
 		
 	private void CalculateDrag () {
@@ -161,139 +122,6 @@ public class AirplaneBehaviour : MonoBehaviour {
 		rb.AddForceAtPosition(-frontDrag * transform.forward, centerOfMassPosition.transform.position);
 		rb.AddForceAtPosition(-upDrag * transform.up, centerOfMassPosition.transform.position);
 		rb.AddForceAtPosition(-sideDrag * transform.right, centerOfMassPosition.transform.position);
-	}
-
-	private float CalculateAngleOfAttack (Vector3 velocity, Vector3 airVelocity, float angleOfAttackOffset) {
-		Vector3 velocity2D;
-		Vector3 airVelocity2D;
-		float angleOfAttack;
-		float angleSign;
-		Vector3 crossProduct;
-
-//		velocity2D = new Vector3 (0, velocity.y, velocity.z);
-//		airVelocity2D = new Vector3 (0, airVelocity.y, airVelocity.z);
-
-//		velocity2D = Quaternion.AngleAxis(-angleOfAttackOffset, transform.right) * velocity2D;
-
-		Vector3 planeNormal = Vector3.Cross(transform.up, transform.forward);
-		planeNormal.Normalize();
-		float distance = -Vector3.Dot(planeNormal, rb.velocity);
-		Vector3 projectedVelocity = rb.velocity + planeNormal * distance;
-
-		angleOfAttack = Vector3.Angle(transform.forward, projectedVelocity);
-		crossProduct = Vector3.Cross(transform.forward, projectedVelocity);
-		angleSign = Mathf.Sign(Vector3.Dot(transform.right, crossProduct));
-		
-		angleOfAttack *= angleSign;
-
-		Debug.DrawLine(wingLiftPosition.transform.position, wingLiftPosition.transform.position + projectedVelocity.normalized * 5, Color.blue);
-		Debug.DrawLine(wingLiftPosition.transform.position, wingLiftPosition.transform.position + transform.forward * 5, Color.white);
-		Debug.DrawLine(wingLiftPosition.transform.position, wingLiftPosition.transform.position + crossProduct.normalized * 5, Color.yellow);
-
-		return angleOfAttack;
-	}
-
-	private float CalculateAngleCoefficient (float angleOfAttack, float criticalAngleOfAttack, bool useFlap) {
-		float angleCoefficient;
-
-		if (useFlap) {
-			criticalAngleOfAttack -= flapCriticalAoAOffset / 2;
-			angleCoefficient = (angleOfAttack + flapCriticalAoAOffset + 5) * 0.07f;
-		}
-		else {
-			angleCoefficient = (angleOfAttack + 5)* 0.07f;
-		}
-			
-		if (angleOfAttack > criticalAngleOfAttack) {
-			float excess = (angleOfAttack - criticalAngleOfAttack) * 0.07f;
-			angleCoefficient -= 2 * excess;
-			if (angleOfAttack > 25)
-				angleCoefficient = 0;
-		}
-		if (angleOfAttack < -criticalAngleOfAttack) {
-			float excess = (angleOfAttack + criticalAngleOfAttack) * 0.07f;
-			angleCoefficient -= 2 * excess;
-			if (angleOfAttack < -25)
-				angleCoefficient = 0;
-		}
-		return angleCoefficient;
-	}
-
-	private void ApplyWingLift () {
-		Vector3 direction;
-		direction = Vector3.Cross(new Vector3(0, rb.velocity.y, rb.velocity.z), transform.right);
-		direction.Normalize();
-
-		Debug.DrawLine(wingLiftPosition.transform.position, wingLiftPosition.transform.position + direction);
-
-		leftWingLift = CalculateLift(leftWingSpeedSensor.AirSpeed(), wingLiftMultiplier, wingLiftCoefficient / 2, leftWingAngleCoefficient);
-		rightWingLift = CalculateLift(rightWingSpeedSensor.AirSpeed(), wingLiftMultiplier, wingLiftCoefficient / 2, rightWingAngleCoefficient);
-
-		if (isFlapOn) {
-			leftWingLift += CalculateLift(leftWingSpeedSensor.AirSpeed(), flapLiftMultiplier, flapLiftCoefficient / 2, leftWingAngleCoefficient);
-			rightWingLift += CalculateLift(rightWingSpeedSensor.AirSpeed(), flapLiftMultiplier, flapLiftCoefficient / 2, rightWingAngleCoefficient);
-		}
-
-		totalLift = leftWingLift + rightWingLift;
-		rb.AddForceAtPosition(totalLift * direction, wingLiftPosition.transform.position);
-
-		//		Debug.Log("Wing Lift: " + totalLift);
-	}
-
-	private void ApplyTailLift () {
-		Vector3 direction;
-		direction = Vector3.Cross(new Vector3(0, rb.velocity.y, rb.velocity.z), transform.right);
-		direction.Normalize();
-		tailLift = CalculateLift(tailWingSpeedSensor.AirSpeed(), tailLiftMultiplier, tailLiftCoefficient, tailWingAngleCoefficient);
-		//		tailLift = transform.InverseTransformDirection(tailLift);
-		rb.AddForceAtPosition(tailLift * direction, tailLiftPosition.transform.position);
-
-		//		Debug.Log("Tail Lift: " + tailLift);
-	}
-
-	public List<float> GetLiftForce() {
-		List<float> liftValues = new List<float>();
-		liftValues.Add(rightWingLift);
-		liftValues.Add(leftWingLift);
-		liftValues.Add(tailLift);
-
-		return liftValues;
-	}
-
-	public List<float> GetDragForce() {
-		List<float> dragValues = new List<float>();
-		dragValues.Add(sideDrag);
-		dragValues.Add(upDrag);
-		dragValues.Add(frontDrag);
-
-		return dragValues;
-	}
-
-	public List<Vector3> GetAirSpeed() {
-		List<Vector3> airSpeedValues = new List<Vector3>();
-		airSpeedValues.Add(leftWingSpeedSensor.AirSpeed());
-		airSpeedValues.Add(rightWingSpeedSensor.AirSpeed());
-		airSpeedValues.Add(tailWingSpeedSensor.AirSpeed());
-
-		return airSpeedValues;
-	}
-
-	public List<float> GetAngleOfAttack() {
-		List<float> aOAValues = new List<float>();
-		aOAValues.Add(leftWingAoA);
-		aOAValues.Add(rightWingAoA);
-		aOAValues.Add(tailWingAoA);
-
-		return aOAValues;
-	}
-
-	public List<float> GetAngleCoefficients() {
-		List<float> coefficientValues = new List<float>();
-		coefficientValues.Add(leftWingAngleCoefficient);
-		coefficientValues.Add(rightWingAngleCoefficient);
-		coefficientValues.Add(tailWingAngleCoefficient);
-
-		return coefficientValues;
 	}
 
 	public float GetEngineLevel() {
---
+++ AeroRace/AeroRace/Assets/Scripts/Airplane/WingBehaviour.cs
@@ -0,0 +1,162 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WingBehaviour : MonoBehaviour {
+
+	[Header("Airplane")]
+	public GameObject airplane;
+
+	[Header("Lift")]
+	public float liftMultiplier = 1;
+	public float liftCoefficient = 6;
+	public float criticalAngleOfAttack = 15;
+	public float angleOffset = 0;
+	public Transform liftPosition;
+	public AirSpeedSensor speedSensorObj;
+
+	[Header("Drag")]
+	public float frontDragCoefficient = 1;
+	public float sideDragCoefficient = 1;
+	public float upDragCoefficient = 1;
+
+	private Rigidbody rb;
+	private Vector3 airSpeed;
+	private float angleOfAttack;
+	private float angleCoefficient;
+	private Vector3 liftForce;
+	private Vector3 dragForce;
+
+	// Use this for initialization
+	void Start () {
+		rb = airplane.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+		airSpeed = speedSensorObj.AirSpeed();
+		angleOfAttack = CalculateAngleOfAttack();
+		angleCoefficient = CalculateAngleCoefficient();
+		liftForce = CalculateLift();
+		dragForce = CalculateParasiticDrag();
+
+		ApplyForces();
+	}
+
+	private float CalculateAngleOfAttack () {
+		float angleOfAttack;
+		float angleSign;
+		Vector3 crossProduct;
+
+		//Project velocity vector into wing forward-up plane
+		Vector3 planeNormal = Vector3.Cross(transform.up, transform.forward);
+		planeNormal.Normalize();
+		float distance = -Vector3.Dot(planeNormal, rb.velocity);
+		Vector3 projectedVelocity = rb.velocity + planeNormal * distance;
+
+		//Calculate angle between projected velocity vector and object forward
+		angleOfAttack = Vector3.Angle(transform.forward, projectedVelocity);
+
+		//Check if angle is positive or negative
+		crossProduct = Vector3.Cross(transform.forward, projectedVelocity);
+		angleSign = Mathf.Sign(Vector3.Dot(transform.right, crossProduct));
+
+		//Apply angle
+		angleOfAttack *= angleSign;
+		angleOfAttack += angleOffset;
+
+//		Debug.DrawLine(liftPosition.transform.position, liftPosition.transform.position + projectedVelocity.normalized * 5, Color.blue);
+//		Debug.DrawLine(liftPosition.transform.position, liftPosition.transform.position + transform.forward * 5, Color.white);
+//		Debug.DrawLine(liftPosition.transform.position, liftPosition.transform.position + crossProduct.normalized * 5, Color.yellow);
+		return angleOfAttack;
+	}
+
+	private float CalculateAngleCoefficient () {
+
+//		if (useFlap) {
+//			criticalAngleOfAttack -= flapCriticalAoAOffset / 2;
+//			angleCoefficient = (angleOfAttack + flapCriticalAoAOffset + 5) * 0.07f;
+//		}
+//		else {
+//			angleCoefficient = (angleOfAttack + 5)* 0.07f;
+//		}
+
+		angleCoefficient = (angleOfAttack + 5)* 0.07f;
+
+		if (angleOfAttack > criticalAngleOfAttack) {
+			float excess = (angleOfAttack - criticalAngleOfAttack) * 0.07f;
+			angleCoefficient -= 2 * excess;
+			if (angleOfAttack > 25)
+				angleCoefficient = 0;
+		}
+		if (angleOfAttack < -criticalAngleOfAttack) {
+			float excess = (angleOfAttack + criticalAngleOfAttack) * 0.07f;
+			angleCoefficient -= 2 * excess;
+			if (angleOfAttack < -25)
+				angleCoefficient = 0;
+		}
+		return angleCoefficient;
+	}
+
+	private Vector3 CalculateLift () {
+		float lift;
+		float inducedDrag;
+		float dragCoefficient;
+		Vector3 resultantForce;
+
+		//Lift
+		if (airSpeed.z >= 0)
+			lift = Mathf.Pow(airSpeed.z, 2) * liftCoefficient * liftMultiplier * 0.5f * angleCoefficient;
+		else
+			lift = 0;
+
+		if (lift > 80000)
+			lift = 80000;
+
+		//Parasitic drag
+		dragCoefficient = (Mathf.Pow(angleOfAttack, 2) * 0.00024f) + 0.01f;
+		inducedDrag = lift * dragCoefficient;
+
+		//Resultant vector
+		resultantForce = new Vector3(0, lift, -inducedDrag);
+
+		return resultantForce;
+	}
+
+	private Vector3 CalculateParasiticDrag () {
+		Vector3 parasiticDrag;
+
+		parasiticDrag.z = Mathf.Pow(airSpeed.z, 2) * frontDragCoefficient * 0.5f;
+		parasiticDrag.y = Mathf.Pow(airSpeed.y, 2) * upDragCoefficient * 0.5f;
+		parasiticDrag.x = Mathf.Pow(airSpeed.x, 2) * frontDragCoefficient * 0.5f;
+
+
+		if (airSpeed.z < 0) parasiticDrag.z *= -1;
+		if (airSpeed.y < 0) parasiticDrag.y *= -1;
+		if (airSpeed.x < 0) parasiticDrag.x *= -1;
+
+		return parasiticDrag;
+	}
+
+	private void ApplyForces () {
+//		Vector3 direction;
+//		direction = Vector3.Cross(rb.velocity, transform.right);
+//		direction.Normalize();
+//
+////		rb.AddForceAtPosition(liftForce.y * direction, liftPosition.position);
+////		rb.AddForceAtPosition(liftForce.z * rb.velocity, liftPosition.position);
+//
+//		Debug.DrawLine(transform.position, transform.position + (liftForce.y * direction).normalized,Color.white);
+//		Debug.DrawLine(transform.position, transform.position + (liftForce.z * rb.velocity).normalized, Color.blue);
+
+		Quaternion rotation = Quaternion.LookRotation(rb.velocity, transform.up);
+		Vector3 forceVector = rotation * liftForce;
+		Debug.DrawLine(transform.position, transform.position + forceVector.normalized);
+
+		rb.AddForceAtPosition(forceVector, liftPosition.position);
+		rb.AddRelativeForce(dragForce);
+	}
+}
